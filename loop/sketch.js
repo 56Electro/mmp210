2b2a08ba195b05cf73282169ca46ce9feee51617
56Electro <a1dude56@gmail.com>
2020-05-07 16:46:07 -0400

    adds color change

--- loop/sketch.js
+++ loop/sketch.js
@@ -1,25 +1,29 @@
-
-// loop repeats something
-for (let i = 100; i >= 1; i--) {
-	console.log('i', i);
-}
-
-/*
-	loop example
-	3.23.2020
-*/
-
 function setup() {
 	createCanvas(640, 360);
 }
 
 function draw() {
 	background('darkblue');
-	strokeWeight(100);
-	stroke(255);
+	
 
-	ellipse(0, 200, 25, 25);
-	ellipse(50, 200, 25, 25);
+   
+  
+    for (var x = 0; x < width; x += 120) {
+      
+      strokeWeight(100);
+	  stroke('#00FFFF');
+      fill(0);
+      ellipse(x, 200, 25, 25);
+      
+      textSize(20);
+      fill('');
+      noStroke();
+      text(x, x, 200);
+    }
+
+  
+  
+  /*
 	ellipse(100, 200, 25, 25);
 	ellipse(150, 200, 25, 25);
 	ellipse(200, 200, 25, 25);
@@ -32,6 +36,7 @@ function draw() {
 	ellipse(550, 200, 25, 25);
 	ellipse(600, 200, 25, 25);
 	ellipse(650, 200, 25, 25);
+    */
 
 
 }
