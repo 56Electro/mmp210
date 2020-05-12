ffd7e51e07baf331c1e3284ac2c723b042818064
56Electro <a1dude56@gmail.com>
2020-05-12 00:17:18 -0400

    adds new pattern

--- pattern/sketch.js
+++ pattern/sketch.js
@@ -3,67 +3,86 @@
 	5/8/2020
 */
 
-var bgSlider;
-var redSlider;
+let myColors = [[80, 80, 80], [248, 85, 86], [248, 143, 75], [253, 241, 133], [141, 214, 221]];
+let i, j, colorFill;
+let title, shapeSel, shuffleButton, input, submButton;
 
-function setup() {
-	createCanvas(500, 500);
-	noStroke();
-
-	createP("Background Color");
-	bgSlider = createSlider(0, 255, 51);
-	bgSlider.input(pattern);
-
-	createP("Red Color");
-	redSlider = createSlider(0, 255, 0);
-	redSlider.input(pattern);
-
-	var patternButton = createButton("Pattern");
-	patternButton.mousePressed(pattern);
-
-	var saveButton = createButton("Save");
-	saveButton.mousePressed(saveImage);
-
-	pattern();
+function setup() {  
+  pageElements();
+  createCanvas(480, 480);
+  noStroke();
+  ellipseMode(CORNER);
 }
 
-function saveImage() {
-	save("pattern.jpg");
+function draw() {
+
 }
 
+function submit() {
+  //Check if the user has enter a name
+  if(input.value() != "") {
+    updateTitle();
+    drawShapes();
+  }
+}
 
-function pattern() {
-	background(bgSlider.value());
+function drawShapes() {
+  background(245, 245, 245);
+  
+  for (i = 0; i < 6; i++) {
+    for (j = 0; j < 6; j++) {
+      fillRandomColor();
+      
+      if (shapeSel.value() == "Circle ◯") {
+        ellipse(i*80+7.5, j*80+7.5, 65, 65);
+      }
+      
+      else if (shapeSel.value() == "Triangle △") {
+        triangle(i*80+40, j*80+10, i*80+10, j*80+70, i*80+70, j*80+70)
+      }
+      
+      //If both shapes are loved, a checkerboard will be drawn
+      else {
+        if ((i/2 == int(i/2) && j/2 != int(j/2)) || (i/2 != int(i/2) && j/2 == int(j/2))) {
+          ellipse(i*80+7.5, j*80+7.5, 65, 65);
+        }
+      
+        else {
+          triangle(i*80+40, j*80+10, i*80+10, j*80+70, i*80+70, j*80+70);
+        }
+      }   
+    }
+  }
+}
 
-	var w = 40; // width of one cell
-	var h = w; // height of one cell
+function fillRandomColor() {
+  colorFill = int(random(0, myColors.length));
+  fill(myColors[colorFill][0], myColors[colorFill][1], myColors[colorFill][2]);
+}
 
-	for (let x = 0; x <= width; x += w) {
-		for (let y = 0; y <= height; y += h) {
-			push();
-			translate(x, y);
+function updateTitle() {
+  if (shapeSel.value() == "Circle" || shapeSel.value()  == "Triangle") {
+    title.html(input.value() + " Loves " + shapeSel.value());
+  }
+  
+  else {
+    title.html(input.value() + " Loves Both! XD");
+  }
+  
+  //Change the Submit button's label
+  submButton.html("Resubmit / Shuffle Colors");
+}
 
-			var r = redSlider.value(); // red
-			var g = random(200, 255);
-			var b = random(50, 150);
-
-			stroke(r, g, b);
-			strokeWeight(5);
-
-			var randomChoice = random(4);
-			if (randomChoice < 1) {
-				line(0, 0, w, h);
-			} else if (randomChoice < 2) {
-				line(0, h, w, h);
-			} else if (randomChoice < 3) {
-				line(w, 0, w, h);
-			} else {
-				line(w, 0, 0, h);
-			}
-			
-			pop();
-		}
-	}
-
-	
+function pageElements() {
+  createP("Enter your name: ");
+  input = createInput("");
+  createP("<br>Select your favorite shape: ");
+  shapeSel = createSelect();
+  shapeSel.option("Circle ○");
+  shapeSel.option("Triangle △");
+  shapeSel.option("I like both");
+  createP("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;")
+  submButton = createButton("Submit");
+  submButton.mousePressed(submit);
+  title = createElement("h1", "");
 }
