5488b58d57f8edf220619697be0c24c1aa517d84
56Electro <a1dude56@gmail.com>
2020-05-22 19:14:22 -0400

    adds dom elements

---
+++ dom/sketch.js
@@ -0,0 +1,33 @@
+/*
+	DOM Elements
+	5.22.2020
+*/
+
+var bgcolor;
+var button;
+var slider;
+var input;
+
+function setup() {
+	canvas = createCanvas(200, 200);
+	bgcolor = color(200);
+	createP('');
+	button = createButton("Click me to change the background!");
+	button.mousePressed (changeColor);
+
+	slider = createSlider(10, 100, 100);
+	input = createInput('Enter any text here!');
+
+}
+
+function changeColor() {
+	bgcolor = color(random(255));
+}
+
+function draw() {
+	background(bgcolor);
+	fill(3, 73, 252);
+	rect(100, 100, slider.value(), slider.value());
+
+	text(input.value(), 10, 20);
+}
